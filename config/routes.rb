10dd5da1abc4a2b7cf0803f87857dbf5cfc43205
atsuko8282 <a.i.flat@hotmail.co.jp>
2020-07-25 20:18:41 +0900

    Add edit function

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   #ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出される
   root "messages#index"
+  resources :users, only: [:edit, :update]
 end
