67072d0489f84c2e7b79c570d5690ee9a486fe15
atsuko8282 <a.i.flat@hotmail.co.jp>
2020-07-27 21:55:10 +0900

    Create groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   #ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出される
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resourses :groups, only: [:new, :create]
 end
