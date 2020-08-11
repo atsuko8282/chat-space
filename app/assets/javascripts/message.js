ae1d7f31d23030563fa2f06dd3f7beb451c9dd21
atsuko8282 <a.i.flat@hotmail.co.jp>
2020-08-11 17:26:59 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  $('.Form').on('submit', function(e){
+    e.preventDefault();
+    let url = $(this).attr('action');
+    let formData = new FormData(this)
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
