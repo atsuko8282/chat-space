2ac0a1c7f54a38fc6286723b8b6746c1641ec24e
atsuko8282 <a.i.flat@hotmail.co.jp>
2020-08-11 20:36:04 +0900

    Add other functions

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,7 +53,12 @@ $(function(){
     .done(function(data){
       let html = buildHTML(data);
       $('.chat-main__message-list').append(html);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $(".send").prop('disabled', false);
       $('form')[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
     })
   });
 });
